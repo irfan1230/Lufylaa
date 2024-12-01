35056273d40b9d5c2d3d8457ba3b43171c84a738
Lothbrock717 <91334582+Lothbrock717@users.noreply.github.com>
2024-12-01 13:56:54 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -8,7 +8,7 @@ class Config(object):
   DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001939577738"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6728038801"))
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "1581901379"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://MRDAXX:MRDAXX@mrdaxx.prky3aj.mongodb.net/?retryWrites=true&w=majority")
   UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001939577738")
   LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001939577738"))
